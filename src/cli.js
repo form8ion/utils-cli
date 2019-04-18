3909be8dc51b956832e9d112e8229f32501728c0
Matt Travi <programmer@travi.org>
2019-04-17 21:33:54 -0500

    fix(command): switched from directory to explicit command
    since rollup doesnt account for the relative directory contents
    
    for #3 and #2

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,10 @@
+import * as scaffoldCommand from './commands/scaffold';
+
 export default function (yargs) {
   return yargs
     .scriptName('form8ion-utils')
     .usage('Usage: $0 <cmd> [args]')
-    .commandDir('commands', {exclude: /^enhanced-scaffolders$/})
+    .command(scaffoldCommand)
     .demandCommand(1, 'You need at least one command before moving on')
     .help('h')
     .alias('h', 'help')
--- test/unit/cli-test.js
+++ test/unit/cli-test.js
@@ -1,6 +1,7 @@
 import sinon from 'sinon';
 import any from '@travi/any';
 import {assert} from 'chai';
+import * as scaffoldCommand from '../../src/commands/scaffold';
 import cli from '../../src/cli';
 
 suite('cli', () => {
@@ -9,12 +10,12 @@ suite('cli', () => {
     const usage = sinon.stub();
     const help = sinon.stub();
     const alias = sinon.stub();
-    const commandDir = sinon.stub();
+    const command = sinon.stub();
     const demandCommand = sinon.stub();
     const argv = any.simpleObject();
     scriptName.withArgs('form8ion-utils').returns({usage});
-    usage.withArgs('Usage: $0 <cmd> [args]').returns({commandDir});
-    commandDir.withArgs('commands', {exclude: /^enhanced-scaffolders$/}).returns({demandCommand});
+    usage.withArgs('Usage: $0 <cmd> [args]').returns({command});
+    command.withArgs(scaffoldCommand).returns({demandCommand});
     demandCommand.withArgs(1, 'You need at least one command before moving on').returns({help});
     help.withArgs('h').returns({alias});
     alias.withArgs('h', 'help').returns({argv});
