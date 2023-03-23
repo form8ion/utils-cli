96086350ec04626636ce0b763848daf0e2db6b74
Matt Travi <programmer@travi.org>
2023-03-23 09:01:30 -0500

    feat(travis-tokens): removed the command to update tokens on travis ci
    since this org no longer uses travis. this also removes the final remaining mocha tests, so cleaned
    up the now unused parts involved with mocha tests
    
    BREAKING CHANGE: the travis-tokens command has been removed since this org no longer uses travis-ci

--- src/commands/travis-tokens.js
+++
@@ -1,8 +0,0 @@
-import {update} from 'travis-token-updater';
-
-export function handler() {
-  return update({githubAccount: 'form8ion'});
-}
-
-export const command = 'travis-tokens';
-export const describe = 'Roll token for Travis projects throughout the organization';
