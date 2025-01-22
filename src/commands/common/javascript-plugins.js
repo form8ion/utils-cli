177b91ce27b9a5ce914b66df3581700bf8348b22
Matt Travi <programmer@travi.org>
2025-01-21 20:07:23 -0600

    refactor(js-plugins): extract the plugins for reuse in the lifter

---
+++ src/commands/common/javascript-plugins.js
@@ -0,0 +1,26 @@
+import * as githubActionsPlugin from '@form8ion/github-actions-node-ci';
+import * as hapiPlugin from '@form8ion/hapi-scaffolder';
+import * as form8ionPlugin from '@form8ion/scaffolder-scaffolder';
+import * as remarkPlugin from '@form8ion/remark-plugin-scaffolder';
+import * as octoherdScriptPlugin from '@form8ion/octoherd-script';
+import * as rollupPlugin from '@form8ion/rollup';
+import * as vitePlugin from '@form8ion/vite';
+
+import {unitTestFrameworks} from './test-frameworks.js';
+
+export function getJavascriptPlugins() {
+  return {
+    ciServices: {'GitHub Actions': githubActionsPlugin},
+    applicationTypes: {Hapi: hapiPlugin},
+    packageTypes: {
+      'form8ion Plugin': form8ionPlugin,
+      'Remark Plugin': remarkPlugin,
+      'Octoherd Script': octoherdScriptPlugin
+    },
+    unitTestFrameworks,
+    packageBundlers: {
+      Rollup: rollupPlugin,
+      Vite: vitePlugin
+    }
+  };
+}
