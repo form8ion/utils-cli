32aceaa8852f00a369d41afa04923264844afd57
Matt Travi <programmer@travi.org>
2026-03-17 23:38:22 -0500

    ci(codecov): report bundle analytics

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import autoExternal from 'rollup-plugin-auto-external';
 import json from '@rollup/plugin-json';
 import executable from 'rollup-plugin-executable';
 import nodeResolve from '@rollup/plugin-node-resolve';
+import {codecovRollupPlugin} from '@codecov/rollup-plugin';
 
 export default {
   input: 'src/index.js',
@@ -10,7 +11,12 @@ export default {
     autoExternal(),
     json(),
     executable(),
-    nodeResolve({mainFields: ['module']})
+    nodeResolve({mainFields: ['module']}),
+    codecovRollupPlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: '@form8ion/utils-cli',
+      uploadToken: process.env.CODECOV_TOKEN
+    })
   ],
   external: ['source-map-support/register', 'yargs/helpers'],
   output: [{file: 'bin/form8ion-utils.js', format: 'esm', sourcemap: true, banner: '#!/usr/bin/env node'}]
